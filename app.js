3ef178dd14cddd5c43ae834c40ade1080b6791a5
AnnaXu21 <mengsinghsu@gmail.com>
2022-06-23 19:02:44 +0800

    upadate

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const Fruit = require('./models/fruit');
+const catchAysnc = require('./utils/catchAsync');
+const ExpressError =require('./utils/ExpressError');
+const { fruitSchema, reviewSchema } = require('./validateSchemas.js');
+const Review  = require('./models/review');
+const fruit = require('./models/fruit');
 
 mongoose.connect('mongodb://localhost:27017/fruitdiscovery');
 
@@ -22,48 +27,105 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
-
+const validateFruit = (req, res, next) =>{
+    // schema to handle errors on server side
+  
+    const { error } = fruitSchema.validate(req.body);
+    if(error){
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400);
+    } else {
+        next();
+    }
+}
+const validateReview = (req, res, next) => {
+    const { error } = reviewSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+}
 app.get('/', (req, res) => {
     res.render('home')
-});
-app.get('/fruits', async (req, res) => {
+})
+
+// display all fruits
+app.get('/fruits', catchAysnc(async (req, res) => {
     const fruits = await Fruit.find({});
     res.render('fruits/index', { fruits })
-});
+}))
+
+// new fruit page
 app.get('/fruits/new', (req, res) => {
     res.render('fruits/new');
 })
 
-app.post('/fruits', async (req, res) => {
+// add new fruit
+app.post('/fruits', validateFruit, catchAysnc(async (req, res, next) => {
+    // if(!req.body.fruit) throw new ExpressError('Invalid fruit data', 400);
+    
     const Fruit = new Fruit(req.body.Fruit);
     await Fruit.save();
     res.redirect(`/fruits/${Fruit._id}`)
-})
+}))
 
-app.get('/fruits/:id', async (req, res,) => {
-    const fruit = await Fruit.findById(req.params.id)
+// find fruit
+app.get('/fruits/:id', catchAysnc(async (req, res,) => {
+    const fruit = await Fruit.findById(req.params.id).populate('reviews');
     res.render('fruits/show', { fruit });
-});
+}))
 
-app.get('/fruits/:id/edit', async (req, res) => {
+// edit fruit
+app.get('/fruits/:id/edit', catchAysnc(async (req, res) => {
     const fruit = await Fruit.findById(req.params.id)
     res.render('Fruits/edit', { fruit });
-})
+}))
 
-app.put('/fruits/:id', async (req, res) => {
+
+// update fruit
+app.put('/fruits/:id', validateFruit, catchAysnc(async (req, res) => {
     const { id } = req.params;
     const fruit = await Fruit.findByIdAndUpdate(id, { ...req.body.fruit });
     res.redirect(`/fruits/${fruit._id}`)
-});
+}))
 
-app.delete('/fruits/:id', async (req, res) => {
+// delete fruit
+app.delete('/fruits/:id', catchAysnc(async (req, res) => {
     const { id } = req.params;
     await Fruit.findByIdAndDelete(id);
     res.redirect('/fruits');
+}))
+
+//submit a review
+app.post('/fruits/:id/reviews', validateReview, catchAysnc(async(req, res) =>{
+    const fruit = await Fruit.findById(req.params.id);
+    const review = new Review(req.body.review);
+    fruit.reviews.push(review);
+    await review.save();
+    await fruit.save();
+    res.redirect(`/fruits/${fruit._id}`);
+}))
+
+//delete a review
+app.delete('/fruits/:id/reviews/:reviewId', catchAysnc( async(req, res) => {
+    const { id, reviewId } = req.params;
+    await Fruit.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/fruits/${id}`);
+}))
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('not found', 404));
 })
 
-
+app.use((err, req, res, next) => {
+    const {status = 500 } = err;
+    if(!err.message) err.message = 'something wrong';
+    res.status(status).render('error', { err });
+})
 
 app.listen(3000, () => {
-    console.log('Serving on port 3000')
+    console.log('Serving on port 3000');
 })
--- models/fruit.js
+++ models/fruit.js
@@ -1,12 +1,28 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const Review = require('./review');
 
 const FruitSchema = new Schema({
     title: String,
     image: String,
     price: Number,
     description: String,
-    location: String
+    location: String,
+    reviews: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Review'
+    }]
 });
 
+// mongoose middleware for deleting review after deleting a fruit
+FruitSchema.post('findOneAndDelete', async function(doc) {
+    if(doc) {
+        await Review.deleteMany({
+            _id: {
+                // all ids in fruit.reviews
+                $in: doc.reviews
+            }
+        })
+    }
+})
 module.exports = mongoose.model('Fruit', FruitSchema);
